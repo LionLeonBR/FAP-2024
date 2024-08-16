16f2c685a1643534591ce02e17657fbedced52f8
LionLeonBR <leonardomoraisjeronimo@gmail.com>
2024-08-16 16:32:09 -0300

    Estudos até 30 minutos do video

--- EstudosPrópios/Kivy Course - Create Python Games and Mobile Apps/Entre 0 e 1 hora/1.py
+++ EstudosPrópios/Kivy Course - Create Python Games and Mobile Apps/Entre 0 e 1 hora/1.py
@@ -1,5 +1,21 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
+
+
+class BoxLayoutExample(BoxLayout):
+    pass
+    # def __init__(self, **kwargs):
+    #     super().__init__(**kwargs)
+    #     self.orientation = "vertical"
+    #     b1 = Button(text="Jojo 1 ")
+    #     b2 = Button(text="Jojo 2 ")
+    #     b3 = Button(text="Jojo 3 ")
+    #     self.add_widget(b1)
+    #     self.add_widget(b2)
+    #     self.add_widget(b3)
+
 
 class MainWidget(Widget): # Interface principal
     pass
